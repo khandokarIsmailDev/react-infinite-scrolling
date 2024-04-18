7417b8757d0c78ac5af049d67b585d5e622f3271
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-19 00:30:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProductList from "./components/ProductList";
+
+export default function App() {
+    return (
+        <div>
+            <h1 className=" text-xl text-center text-purple-700">React Infinite Scrolling</h1>
+
+            <ProductList />
+        </div>
+    );
+}
---
+++ src/components/Sekelon.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Sekelon = () => {
+    return (
+        <div className="flex gap-5 justify-between mt-5">
+            <div className="card is-loading">
+            <div className="image-skelton" />
+            <div className="content">
+              <h2 />
+              <p />
+            </div>
+          </div>
+          <div className="card is-loading">
+            <div className="image-skelton" />
+            <div className="content">
+              <h2 />
+              <p />
+            </div>
+          </div>
+          <div className="card is-loading">
+            <div className="image-skelton" />
+            <div className="content">
+              <h2 />
+              <p />
+            </div>
+          </div>
+        </div>
+    );
+};
+
+export default Sekelon;
